0405f23f7a0c726f67726827ee1e26a214d4afb9
Alina <160795966+alrozental@users.noreply.github.com>
2026-04-14 12:01:14 +0200

    Update NoteForm.tsx

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -20,7 +20,7 @@ const initialValues: InitialValues = {
 const NoteSchema = Yup.object().shape({
   title: Yup.string()
     .min(3, "Title must be at least 3 characters")
-    .max(30, "Title must be at most 30 characters")
+    .max(50, "Title must be at most 50 characters")
     .required("Title is required"),
   content: Yup.string().max(500, "Content must be at most 500 characters"),
   tag: Yup.string()
